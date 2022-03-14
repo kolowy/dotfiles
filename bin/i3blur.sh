e47fb26bc8136bb9b0f096fe04ca52a1e36bc7be
paul Dufour <paul.dufour@epita.fr>
2022-03-14 15:11:31 +0100

    [ADD] first dot

---
+++ bin/i3blur.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+icon="/home/paul/.config/bin/lock.png"
+img=$(mktemp /tmp/XXXXXXXXX.png)
+
+import -window root $img
+convert $img -scale 5% -scale 2000% $img
+convert $img $icon -fill black -gravity center -composite $img
+
+# suspend the message display
+pkill -u "$USER" -USR1 dunst
+
+i3lock -f -n --indicator -k --inside-color=ffffff1c --insidever-color=0000001c \
+        --insidewrong-color=0000001c --time-color=ffffff99 --date-color=ffffff99 \
+        --verif-color=aaaaff99 --wrong-color=ffaaaa99 -i $img
+
+# resume message
+pkill -u "$USER" -USR2 dunst
+
+rm $img
