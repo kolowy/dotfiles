16dafffa00a06e3a3957a5c74790b9a5a0017922
paul Dufour <paul.dufour@epita.fr>
2022-06-11 12:03:14 +0000

    add - rofi

---
+++ .config/rofi/applets/applets/mpd.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/applets/style.sh)"
+
+dir="$HOME/.config/rofi/applets/applets/configs/$style"
+rofi_command="rofi -theme $dir/mpd.rasi"
+
+# Gets the current status of mpd (for us to parse it later on)
+status="$(playerctl status)"
+# Defines the Play / Pause option content
+if [[ $status == *"[playing]"* ]]; then
+    play_pause=""
+else
+    play_pause=""
+fi
+active=""
+urgent=""
+
+# Display if repeat mode is on / off
+tog_repeat=""
+if [[ $status == *"repeat: on"* ]]; then
+    active="-a 4"
+elif [[ $status == *"repeat: off"* ]]; then
+    urgent="-u 4"
+else
+    tog_repeat=" Parsing error"
+fi
+
+# Display if random mode is on / off
+tog_random=""
+if [[ $status == *"random: on"* ]]; then
+    [ -n "$active" ] && active+=",5" || active="-a 5"
+elif [[ $status == *"random: off"* ]]; then
+    [ -n "$urgent" ] && urgent+=",5" || urgent="-u 5"
+else
+    tog_random=" Parsing error"
+fi
+stop=""
+next=""
+previous=""
+
+# Variable passed to rofi
+options="$previous\n$play_pause\n$stop\n$next"
+
+
+
+# Get the current playing song
+current=$(playerctl metadata title)
+# If mpd isn't running it will return an empty string, we don't want to display that
+if [[ -z "$current" ]]; then
+    current="-"
+fi
+
+# Spawn the mpd menu with the "Play / Pause" entry selected by default
+chosen="$(echo -e "$options" | $rofi_command -p "  $current" -dmenu $active $urgent -selected-row 1)"
+case $chosen in
+    $previous)
+        playerctl previous && notify-send -u low -t 1800 " $(mpc current)"
+        ;;
+    $play_pause)
+        playerctl play-pause && notify-send -u low -t 1800 " $(mpc current)"
+        ;;
+    $stop)
+        playerctl stop
+        ;;
+    $next)
+        playerctl next && notify-send -u low -t 1800 " $(mpc current)"
+        ;;
+esac
