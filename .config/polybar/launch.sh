5c6db22a1f60b6fc19c4fe97a2a98f678037fcd9
paul Dufour <paul.dufour@epita.fr>
2022-10-24 14:46:47 +0200

    update for mutiple display

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,14 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
 
 #!/usr/bin/env bash
 
